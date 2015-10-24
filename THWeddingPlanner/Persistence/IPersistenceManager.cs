e11a393037e4d9f865f2aa98be05073f510d3053
Ytrog <hisbrucker@gmail.com>
2015-10-24 23:31:02 +0200

    Added IPersistenceManager interface

---
+++ THWeddingPlanner/Persistence/IPersistenceManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using THWeddingPlanner.Data;
+
+namespace THWeddingPlanner.Persistence
+{
+    interface IPersistenceManager
+    {
+        void Save(WeddingPlan weddingPlan);
+        void Save(WeddingPlan weddingPlan, string path);
+        WeddingPlan Load();
+        WeddingPlan Load(string path);
+
+    }
+}
